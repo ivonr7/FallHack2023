80b6cee8d0d9567bd91ddc038bfa6edb8a59c481
Jun Park <pch9192@gmail.com>
2023-10-21 11:28:31 -0700

    basic structure fix1

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const chatMessages = document.getElementById('chat-messages');
 function appendMessage(message, isUser) {
     const messageContainer = document.createElement('div');
     messageContainer.className = isUser ? 'user-message' : 'bot-message';
-    messageContainer.textContent = message;
+    const userName = isUser ? 'You' : 'ChatBot'; // Determine the username
+    messageContainer.textContent = `${userName}: ${message}`; // Include the username with the message
     chatMessages.appendChild(messageContainer);
 }
 
@@ -13,7 +14,17 @@ function processUserInput() {
     const userMessage = userInput.value;
     appendMessage(userMessage, true);
 
-    // Your chat bot logic here
+    // Generate a random response from the bot
+    const randomResponses = [
+        "Hello there!",
+        "How can I assist you?",
+        "Nice to meet you!",
+        "What's on your mind?",
+        "I'm here to help.",
+    ];
+
+    const randomResponse = randomResponses[Math.floor(Math.random() * randomResponses.length)];
+    appendMessage(randomResponse, false);
 
     userInput.value = '';
 }
@@ -27,7 +38,6 @@ clearChatButton.addEventListener('click', function () {
 function clearChat() {
     chatMessages.innerHTML = ''; // This clears all the content inside the chat-messages container.
 }
-
 
 userInput.addEventListener('keyup', function (event) {
     if (event.key === 'Enter') {
